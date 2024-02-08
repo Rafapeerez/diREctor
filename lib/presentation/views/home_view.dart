801b5fdc6ae408ef0d787f2bfa626e0e93580416
Rafa Perez <rapeop1c@gmail.com>
2024-02-08 18:20:16 +0100

    feat(Card): Create custom card

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:director_app_tfg/presentation/widgets/custom_appbar.dart';
+import 'package:director_app_tfg/presentation/widgets/custom_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,6 +9,11 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(),
+      body: Column(
+        children: [
+          CustomCard(title: 'Concierto', isAttendingEvent: false,/*  image:'lib/config/assets/images/PalmSunday.jpg',description: 'asdasdadasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss', */)
+        ],
+      ),
     );
   }
 }
---
+++ lib/presentation/widgets/custom_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+  final bool isAttendingEvent;
+
+
+  const CustomCard({
+    super.key,
+    required this.title,
+    this.description = "",
+    this.image = "",
+    required this.isAttendingEvent
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        margin: const EdgeInsets.all(15),
+        elevation: 10,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Column(
+            children: <Widget>[
+              image != "" ? Image.asset(image) : const SizedBox(),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      description,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    isAttendingEvent ? _DecitionAttendingButton() : const SizedBox(height: 5)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+
+class _DecitionAttendingButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FilledButton(
+          onPressed: (){}, 
+          style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.green),
+          ),
+          child: const Text('Asisto'),
+        )
+      ],
+    );
+  }
+}
