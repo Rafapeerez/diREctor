8faee7930ab4e4a15229235b16c573d9c6ca2368
Rafa Perez <rapeop1c@gmail.com>
2024-07-02 12:21:00 +0200

    refactor(waiting screen): changed view to avoid misunderstandings with waits

--- lib/presentation/views/waiting_view.dart
+++ lib/presentation/views/waiting_view.dart
@@ -1,67 +1,26 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-class WaitingView extends StatefulWidget {
-  const WaitingView({
-    super.key,
-  });
-
-  @override
-  State<WaitingView> createState() => _WaitingViewState();
-}
-
-class _WaitingViewState extends State<WaitingView> {
-
-  final List<String> _quotes = [
-    "Ponte en contacto con el director...",
-    "Cargando...",
-    "Preparando la información...",
-    "Procesando...",
-  ];
-  String _currentQuote = "";
-  late Timer _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _updateQuote();
-    _timer = Timer.periodic( const Duration(seconds: 5), (timer) {
-      _updateQuote();
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  void _updateQuote() {
-    setState(() {
-      _currentQuote = _quotes[Random().nextInt(_quotes.length)];
-    });
-  }
+class WaitingView extends StatelessWidget {
+  const WaitingView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [
+        children: const [
             Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const SizedBox(height: 40),
-              const Text(
+              SizedBox(height: 40),
+              Text(
                 "¡¡¡Bienvenido!!!",
                 style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold
                 ),
               ),
-              const SizedBox(height: 60),
-              const Padding(
+              SizedBox(height: 60),
+              Padding(
                 padding: EdgeInsets.all(12.0),
                 child: Text(
                   "Esta es la aplicación oficial de la A.M. Nuestro Padre Jesús de la Redención de Córdoba.",
@@ -72,30 +31,16 @@ class _WaitingViewState extends State<WaitingView> {
                 ),
               ),
 
-              const SizedBox(height: 30),
-              const Padding(
+              SizedBox(height: 30),
+              Padding(
                 padding: EdgeInsets.all(12.0),
                 child: Text(
-                  "Espere a que alguien de la junta directiva valide la cuenta y pueda acceder a la app...",
+                  "Cuando la junta directiva valide la cuenta podrá acceder al contenido de la aplicación. Rogamos paciencia.",
                   textAlign: TextAlign.justify,
                   style: TextStyle(
                     fontSize: 20,
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 120),
-              const Center(
-                child: CircularProgressIndicator.adaptive(
-                  strokeWidth: 3,
-                )
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                _currentQuote, 
-                style: const TextStyle(
-                  fontSize: 12
-                )
               ),
             ],
           ),
